4673d9c652b0f3d324d68ede750677e15f62c821
Daniel Diaz <147693977+Dramenable@users.noreply.github.com>
2026-03-14 19:26:07 -0600

    Agregar modelo Producto y su DbSet en ApplicationDbContext

---
+++ CrudContactosMVC/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudContactosMVC.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "El precio debe ser mayor o igual a cero")]
+        public decimal Precio { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor o igual a cero")]
+        public int Cantidad { get; set; }
+    }
+}
